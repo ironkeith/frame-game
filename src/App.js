d05315fc6bec724b0aaf206fb43cf50676c3f266
Keith Silgard <keith@thesilgards.com>
2019-03-13 19:35:42 -0600

    Putting things together to see if they work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Stage from './Stage.js';
+import Target from './Target.js';
+import Tick from './Tick.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Stage velocity={60}>
+          {stageDimensions => (
+            <Tick onTick={() => console.log('TICK')} fps={1}>
+              <Target boundaries={stageDimensions} />
+            </Tick>
+          )}
+        </Stage>
       </div>
     );
   }
